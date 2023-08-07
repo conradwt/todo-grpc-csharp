6cec7835f71a964d51b16574d0f9730a2ed12151
Conrad Taylor <conradwt@gmail.com>
2023-08-06 17:44:05 -0700

    Initial commit.

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+namespace ToDoGrpc.Models;
+
+public  class ToDoItem
+{
+  public int Id { get; set; }
+  public string? Title { get; set; }
+  public string? Description { get; set; }
+  public string ToDoStatus { get; set; } = "NEW";
+}
