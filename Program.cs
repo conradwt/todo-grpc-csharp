3f5602c5090fc0f2a57cf61f0448bf855e9b1d99
Conrad Taylor <conradwt@gmail.com>
2023-08-06 19:47:28 -0700

    Add support creating and reading a todo item.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddGrpc();
 var app = builder.Build();
 
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ToDoService>();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ Services/ToDoService.cs
@@ -0,0 +1,57 @@
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using ToDoGrpc.Data;
+using ToDoGrpc.Models;
+
+namespace ToDoGrpc.Services;
+
+public class ToDoService : ToDoIt.ToDoItBase {
+  private readonly AppDbContext _dbContext;
+
+  public ToDoService(AppDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+
+  public override async Task<CreateToDoResponse> CreateToDo(CreateToDoRequest request, ServerCallContext context)
+  {
+    if (request.Title == string.Empty || request.Description == string.Empty)
+      throw new RpcException(new Status(StatusCode.InvalidArgument, "You must supply a valid object"));
+
+
+    var toDoItem = new ToDoItem
+    {
+      Title = request.Title,
+      Description = request.Description,
+    };
+
+    await _dbContext.AddAsync(toDoItem);
+    await _dbContext.SaveChangesAsync();
+
+    return await Task.FromResult(new CreateToDoResponse
+    {
+      Id = toDoItem.Id
+    });
+  }
+
+  public override async Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+  {
+    if (request.Id <= 0)
+      throw new RpcException(new Status(StatusCode.InvalidArgument, "resource index must be greater than 0"));
+
+
+    var toDoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+
+    if (toDoItem != null) {
+      return await Task.FromResult(new ReadToDoResponse
+      {
+        Id = toDoItem.Id,
+        Title = toDoItem.Title,
+        Description = toDoItem.Description,
+        ToDoStatus = toDoItem.ToDoStatus
+      });
+    }
+
+    throw new RpcException(new Status(StatusCode.NotFound, $"No task with id {request.Id}"));
+  }
+}
